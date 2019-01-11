fa56606f80ee6000c2091c5a734c2d16d68aa76e
Jumee-LDN <jmlregister@gmail.com>
2019-01-11 19:24:22 +0000

    initial title section done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 
+const $title = $('.title').eq(0);
+// const title = document.getElementsByClassName('title')[0];
+var windw = this;
+
+$.fn.followTo = function ( pos ) {
+  var $this = this,
+    $window = $(windw);
+
+  $window.scroll(function(){
+    if ($window.scrollTop() > pos) {
+      $this.css({
+        position: 'absolute',
+        top: pos
+      });
+    } else {
+      $this.css({
+        position: 'fixed',
+        top: 0
+      });
+    }
+  });
+};
+
+$title.followTo(1380);
