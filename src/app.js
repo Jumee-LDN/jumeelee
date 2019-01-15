56f7508186e84a223e786d23bca74edff6aca348
Jumee-LDN <jmlregister@gmail.com>
2019-01-15 01:23:17 +0000

    first project content set

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ const $lifeContentBox = $('.life-contents-box');
 const $contactKeywordBox = $('.contact-keyword-box');
 const $contactContentBox = $('.contact-contents-box');
 const $arrow = $('.arrow');
+const $hoverKeywordContainer = $('.hover-keyword-container');
+
 const windw = this;
 
 $.fn.siteTitleEffect = function ( pos ) {
@@ -43,6 +45,7 @@ const titleToUpperCase = () => {
 };
 titleToUpperCase();
 
+// ABOUT SECTION
 $aboutKeywordBox.click(
   () => {
     $aboutContentBox.toggle();
@@ -50,8 +53,8 @@ $aboutKeywordBox.click(
 );
 $aboutKeywordBox.hover(
   ()=> {
-    $arrow[0].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/right-arrow-green.png';
-  }, 
+    $arrow[0].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/click-green.png';
+  },
   ()=> {
     $arrow[0].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/right-arrow.png';
   } );
@@ -60,56 +63,108 @@ $aboutContentBox.click(
     $aboutContentBox.toggle();
   }
 );
-
+// NATURE SECTION
 $natureKeywordBox.click(
   () => {
     $natureContentBox.toggle();
   }
 );
+$natureKeywordBox.hover(
+  ()=> {
+    $arrow[1].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/click-green.png';
+  },
+  ()=> {
+    $arrow[1].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/right-arrow.png';
+  });
+// $natureKeywordBox.mouseenter(
+//   ()=> {
+//     $hoverKeywordContainer.toggle();
+//     $natureKeywordBox.css({
+//       'background-color': '#00ff66'
+//     });
+//   }
+// );
+// $natureKeywordBox.mouseleave(
+//   ()=> {
+//     $hoverKeywordContainer.toggle();
+//     $natureKeywordBox.css({
+//       color: '#181818',
+//       'background-color': 'white'
+//     });
+//   }
+// );
 $natureContentBox.click(
   () => {
     $natureContentBox.toggle();
   }
 );
-
+// FOOD SECTION
 $foodKeywordBox.click(
   () => {
     $foodContentBox.toggle();
   }
 );
+$foodKeywordBox.hover(
+  ()=> {
+    $arrow[2].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/click-green.png';
+  },
+  ()=> {
+    $arrow[2].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/right-arrow.png';
+  });
 $foodContentBox.click(
   () => {
     $foodContentBox.toggle();
   }
 );
-
+// ART SECTION
 $artKeywordBox.click(
   () => {
     $artContentBox.toggle();
   }
 );
+$artKeywordBox.hover(
+  ()=> {
+    $arrow[3].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/click-green.png';
+  },
+  ()=> {
+    $arrow[3].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/right-arrow.png';
+  });
 $artContentBox.click(
   () => {
     $artContentBox.toggle();
   }
 );
-
+// LIFE SECTION
 $lifeKeywordBox.click(
   () => {
     $lifeContentBox.toggle();
   }
 );
+$lifeKeywordBox.hover(
+  ()=> {
+    $arrow[4].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/click-green.png';
+  },
+  ()=> {
+    $arrow[4].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/right-arrow.png';
+  });
 $lifeContentBox.click(
   () => {
     $lifeContentBox.toggle();
   }
 );
-
+// CONTACT SECTION
 $contactKeywordBox.click(
   () => {
     $contactContentBox.toggle();
   }
 );
+$contactKeywordBox.hover(
+  ()=> {
+    $arrow[5].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/click-green.png';
+  },
+  ()=> {
+    $arrow[5].src = 'file:///Users/jumeelee/portfolio/jumeelee/src/images/right-arrow.png';
+  });
 $contactContentBox.click(
   () => {
     $contactContentBox.toggle();
